b870caad15ccc1b3255843eb9a2e3e6fc2970432
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2023-11-07 08:49:03 -0300

    src/main/java/br/edu/ifpi/enums/Aluno.java

---
+++ src/main/java/br/edu/ifpi/enums/Aluno.java
@@ -0,0 +1,42 @@
+package br.edu.ifpi.enums;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aluno {
+    private String nome;
+    private int numeroMatricula;
+    private List<Curso> cursosMatriculados;
+
+
+    public Aluno(String nome, int numeroMatricula, String informacoesContato) {
+        this.nome = nome;
+        this.numeroMatricula = numeroMatricula;
+        this.cursosMatriculados = new ArrayList<>();
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(int numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public List<Curso> getCursosMatriculados() {
+        return cursosMatriculados;
+    }
+
+    public void setCursosMatriculados(List<Curso> cursosMatriculados) {
+        this.cursosMatriculados = cursosMatriculados;
+    }
+}
