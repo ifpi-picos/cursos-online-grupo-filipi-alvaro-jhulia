acf8d1552bbf28c80725905c03448cd0067d52cb
jhulialeal <lealjhu@gmail.com>
2023-11-20 00:03:53 -0300

    criando conexao com bd e desenvolvendo teste

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -1,11 +1,29 @@
 package br.edu.ifpi;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+
+import br.edu.ifpi.dao.Conexao;
+import br.edu.ifpi.dao.ProfessorDao;
+import br.edu.ifpi.entidades.Professor;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Sistema de cursos online!");
+
+        Connection conexao = Conexao.getConexao();
+
+        ProfessorDao professorDao = new ProfessorDao(conexao);
+
+        String nome = JOptionPane.showInputDialog("Nome: ");
+        String email = JOptionPane.showInputDialog("Email: ");
+
+        Professor novoProfessor = new Professor(nome, email);
+
+        int retorno = professorDao.cadastrar(novoProfessor);
+
+        String mensagem = retorno > 0 ? "Sucesso!" : "Falhou!";
+
+        JOptionPane.showMessageDialog(null, mensagem, "Retorno", JOptionPane.INFORMATION_MESSAGE);
     }
 }
---
+++ src/main/java/br/edu/ifpi/DAO/Conexao.java
@@ -0,0 +1,21 @@
+
+package br.edu.ifpi.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    
+    public static Connection getConexao() {
+        try {
+            Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistema_academico",
+            "root", "12345678");
+            return conexao;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+}
