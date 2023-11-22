63946eb06f2b4da2ccb979859c1f5c95ca403a00
jhulialeal <lealjhu@gmail.com>
2023-11-22 03:06:32 -0300

    configurações para testes locais

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -5,14 +5,11 @@ import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
-import br.edu.ifpi.dao.AlunoDao;
 import br.edu.ifpi.dao.Conexao;
 import br.edu.ifpi.dao.CursoDao;
 import br.edu.ifpi.dao.ProfessorDao;
-import br.edu.ifpi.entidades.Aluno;
 import br.edu.ifpi.entidades.Curso;
 import br.edu.ifpi.entidades.Professor;
-import br.edu.ifpi.enums.StatusAluno;
 import br.edu.ifpi.enums.StatusCurso;
 
 public class App {
@@ -20,42 +17,6 @@ public class App {
         System.out.println("Sistema de cursos online!");
 
         Connection conexao = Conexao.getConexao();
-
-        // ProfessorDao professorDao = new ProfessorDao(conexao);
-
-        // String nome = JOptionPane.showInputDialog("Nome: ");
-        // String email = JOptionPane.showInputDialog("Email: ");
-
-        // Professor novoProfessor = new Professor(nome, email);
-
-        // int retorno = professorDao.cadastrar(novoProfessor);
-
-        // String mensagem = retorno > 0 ? "Sucesso!" : "Falhou!";
-
-        // JOptionPane.showMessageDialog(null, mensagem, "Retorno", JOptionPane.INFORMATION_MESSAGE);
-        // professorDao.consultarTodos();
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-        // AlunoDao alunoDao = new AlunoDao(conexao);
-        // String nome = JOptionPane.showInputDialog("Nome do aluno: ");
-        // String email = JOptionPane.showInputDialog("Email do aluno: ");
-        // StatusAluno status = JOptionPane.showConfirmDialog(null, "O aluno está ativo? ", "Status", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION ? StatusAluno.ATIVO : StatusAluno.INATIVO;
-
-        // Aluno novoAluno = new Aluno(nome, email, status);
-
-        // int retorno = alunoDao.cadastrar(novoAluno);
-
-        // String mensagem = retorno > 0 ? "Cadastro do aluno realizado com sucesso!" : "Falha ao cadastrar o aluno.";
-
-        // JOptionPane.showMessageDialog(null, mensagem, "Retorno", JOptionPane.INFORMATION_MESSAGE);
-        // alunoDao.consultarTodos();
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-
 
         ProfessorDao professorDao = new ProfessorDao(conexao);
 
@@ -72,13 +33,6 @@ public class App {
 
         String mensagem = retorno > 0 ? "Cadastro do curso realizado com sucesso!" : "Falha ao cadastrar o curso.";
         JOptionPane.showMessageDialog(null, mensagem, "Retorno", JOptionPane.INFORMATION_MESSAGE);
-        
-
-
-
-
-
-
 
     }
 
